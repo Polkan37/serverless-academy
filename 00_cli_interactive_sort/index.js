ba4b65524aaa79bbafe05ba25ca87409681f2e0d
Polkan37 <zaq2587413699@gmial.com>
2023-02-07 15:39:18 +0200

    fix: task 00 add max length for input

--- 00_cli_interactive_sort/index.js
+++ 00_cli_interactive_sort/index.js
@@ -1,4 +1,4 @@
-const readline = require('readline').createInterface({
+const readline = require("readline").createInterface({
   input: process.stdin,
   output: process.stdout,
 });
@@ -9,55 +9,59 @@ const options = {
   d: sortWordsByLength,
   e: getUniqWords,
   f: getUniqValues,
-  exit: closeInput
+  exit: closeInput,
 };
+const question = 'Enter from 2 to 10 words or digits dividing them in space: ';
+const optionsQuestion = '\033[1mPls select option:\033[0m \na. Sort words alphabetically \nb. Show numbers from lesser to greater\nc. Show numbers from bigger to smaller\nd. Display words in ascending order by number of letters in the word\ne. Show only unique words\nf. Display only unique values from the set of words and numbers\n   or type \'exit\' for exit\n *write only letter: ';
 
-(function getString(message) {
-  readline.question(message, str => {
-    if (str === 'exit') {
-      return readline.close();
+(function getInput(message) {
+  readline.question(message, (str) => {
+    if (str === "exit") {
+      process.exit();
     }
-    const elements = str.replace(/\s+/g, ' ').trim().split(' ');
+    const elements = cutRedundantSpaces(str);
 
-    elements.length > 1 ?
-      readline.question(`Pls select option: \n
-    a. Sort words alphabetically
-    b. Show numbers from lesser to greater
-    c. Show numbers from bigger to smaller
-    d. Display words in ascending order by number of letters in the word
-    e. Show only unique words
-    f. Display only unique values from the set of words and numbers\n
-    or type 'exit' for exit
-    *write only letter: `, option => {
-        const obj = {
-          allElements: elements,
-          words: elements.filter(el => isNaN(Number(el))),
-          numbers: elements.filter(el => !isNaN(Number(el)))
-        }
-        console.log(options[option](obj));
-        getString(`Enter 10 words or digits dividing them in space: `);
-      }) : getString(`Pls, enter more than 1 number or word dividing by space: `)
+    elements.length > 1 && elements.length <= 10
+      ? readline.question(optionsQuestion, (option) => {
+          if (options[option]) {
+            const obj = {
+              allElements: elements,
+              words: elements.filter((el) => isNaN(Number(el))),
+              numbers: elements.filter((el) => !isNaN(Number(el))),
+            };
+            console.log(options[option](obj));
+            getInput(question);
+          } else {
+            console.log("\033[1mWrong input, try again\033[0m");
+            getInput(question);
+          }
+        })
+      : getInput(question);
   });
-})(`Hello, enter 10 words or digits dividing them in space: `);
+})('Hello, ' + question);
 
 function sortWords(input) {
-  return input.words.sort()
+  return input.words.sort();
 }
 function sortNumbersAscending(input) {
-  return input.numbers.sort((a, b) => a - b)
+  return input.numbers.sort((a, b) => a - b);
 }
 function sortNumbersDescending(input) {
-  return input.numbers.sort((a, b) => b - a)
+  return input.numbers.sort((a, b) => b - a);
 }
 function sortWordsByLength(input) {
-  return input.words.sort((a, b) => a.length - b.length)
+  return input.words.sort((a, b) => a.length - b.length);
 }
 function getUniqWords(input) {
-  return input.words.filter((v, i, a) => a.indexOf(v) === i)
+  return input.words.filter((v, i, a) => a.indexOf(v) === i);
 }
 function getUniqValues(input) {
-  return input.allElements.filter((v, i, a) => a.indexOf(v) === i)
+  return input.allElements.filter((v, i, a) => a.indexOf(v) === i);
 }
 function closeInput() {
-  readline.close()
+  process.exit();
+}
+
+function cutRedundantSpaces(string) {
+  return string.replace(/\s+/g, " ").trim().split(" ");
 }
