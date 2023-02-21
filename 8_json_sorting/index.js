8b76f3f6c2d0fe70aa41e42b1457a90b204dc917
Polkan37 <zaq2587413699@gmail.com>
2023-02-21 23:00:58 +0200

    fix: async reading, 8 task done

--- 8_json_sorting/index.js
+++ 8_json_sorting/index.js
@@ -2,37 +2,29 @@ const fs = require("fs");
 const endpoints = fs
   .readFileSync("./endpoints.txt", { encoding: "utf8", flag: "r" })
   .split(/\r?\n/);
-const data = [];
 
 const fetch_retry = (url, n) =>
   fetch(url).catch(function (error) {
     if (n === 1)
       throw console.log(`[Fail] ${url}: The endpoint is unavailable`);
     return fetch_retry(url, n - 1);
-  });
-
-async function getAllResponses() {
-  let trueValues = 0,
-    falseValues = 0;
-
-  endpoints.forEach(async (url) => {
-    const response = await fetch_retry(url, 3);
-    const result = await response.json();
-    const isDoneValue = findKey(result, "isDone");
-    isDoneValue ? trueValues ++ : falseValues ++;
-    console.log(`[Success] ${url}: isDone - ${isDoneValue}`);
-    console.log('trueValues', trueValues)
-    console.log('falseValues', falseValues)
-  });
-
-  return [trueValues, falseValues];
-}
+  }); 
 
 (async () => {
-  const counters = await getAllResponses();
+  let trueValues = 0, falseValues = 0;
 
-  console.log( `\n Found True values: ${counters[0]}\n Found False values: ${counters[1]}` );
-})();
+  await Promise.all(
+    endpoints.map(async (url) => {
+      const response = await fetch_retry(url, 3);
+      const result = await response.json();
+      const isDoneValue = findKey(result, "isDone");
+      isDoneValue ? trueValues++ : falseValues++;
+      console.log(`[Success] ${url}: isDone - ${isDoneValue}`);
+    })
+  )
+  console.log(`\n Found True values: ${trueValues}\n Found False values: ${falseValues}`);
+})()
+
 
 function findKey(obj, searchingKey) {
   if (obj[searchingKey] !== undefined) {
